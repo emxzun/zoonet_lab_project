c72aecab1848a987eb823105d046a55d9bbbda6b
emir <e352709@gmail.com>
2023-04-10 19:01:16 +0600

    commit

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='zoonet',
+        default_version='v1',
+        description='Swagger documentation'
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/account/', include('applications.account.urls')),
+    path('swagger/', schema_view.with_ui('swagger')),
 ]
