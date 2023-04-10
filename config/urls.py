cbb463f7ba9389f623c7c927b1287d69a0a3fbe6
emir <e352709@gmail.com>
2023-04-10 19:49:22 +0600

    commit

--- config/urls.py
+++ config/urls.py
@@ -31,5 +31,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/account/', include('applications.account.urls')),
-    path('swagger/', schema_view.with_ui('swagger')),
+    path('swagger/', schema_view.with_ui('swagger'))
 ]
