1b463763e769ef1e9209f1238453bc81ee9396d9
emir <e352709@gmail.com>
2023-04-27 20:44:11 +0600

    commit

--- applications/account/send_mail.py
+++ applications/account/send_mail.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 def send_confirmation_email(email, code):
     send_mail(
         'Подтверждение',
-        f'https://zoonet.onrender.com/api/v1/account/activate/{code}',
+        f'https://zoonet-dpu6.onrender.com/api/v1/account/activate/{code}',
         'e352709@gmail.com',
         [email]
     )
